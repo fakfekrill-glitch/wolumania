5bc15a16839e68117a99878add5a2dfd339ad579
fakfekrill-glitch <fakfekrill@gmail.com>
2026-01-13 17:19:45 +0700

    Update background image URL in index.php

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ header("X-Content-Type-Options: nosniff");
 define('SECURE_ACCESS', true);
 
 // --- KONFIGURASI GAMBAR (Global) ---
-$bg_home = "https://www.dbl.id/uploads/post/2025/09/1.jpg"; 
+$bg_home = "https://www.dbl.id/uploads/post/2025/09/11/Wolumania%20kala%20merapatkan%20barisan%20dan%20menyanyikan%20Anthem.jpg"; 
 $img_about = "https://media.discordapp.net/attachments/904596578961096785/1460569234403758244/Tim_basket_SMAN_8_Surabaya_dan_Wolumania_saat_menyanyikan_anthem.jpg?ex=69676499&is=69661319&hm=a1e68c1ba3e677ab7d161638e7e856043d138fbaed5ddb0f09e4ba1c75759287&=&format=webp&width=1423&height=658"; 
 $img_history = "https://images.unsplash.com/photo-1526666923127-b2970f64b422?w=500"; 
 // Gambar Join (Array URL langsung diproses di page join, atau define disini juga boleh)
@@ -39,3 +39,4 @@ if (in_array($page, $allowed_pages)) {
 include __DIR__ . '/../includes/footer.php';
 
 ?>
+
